3055ba4b04104e07e56f9ece20b09cb76d88fae2
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-14 17:47:16 +0900

    카카오 로그인 리다이랙트 url 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -154,7 +154,8 @@ export const startKakaoLogin = (req, res) => {
   const baseUrl = "https://kauth.kakao.com/oauth/authorize";
   const config = {
     client_id: "6b6c7b0bc06af6815119ea637762e914",
-    redirect_uri: "http://localhost:4000/users/kakao/finish",
+    redirect_uri:
+      "https://port-0-clone-youtube-nx562olf43veth.sel3.cloudtype.app/users/kakao/finish",
   };
 
   const params = new URLSearchParams(config).toString();
